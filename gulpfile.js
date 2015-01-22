aea9d23d0f361b855289129b054b8ae98bf4cd97
Frank Wallis <fwallis@outlook.com>
2015-01-22 00:08:20 +0000

    feat(gulp-hub): add the remarkable gulp-hub

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var gutil = require("gulp-util");
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
+var hub = require('gulp-hub');
 
 gulp.task('test', function(cb) {
     gulp.src('lib/**/*.js')
@@ -14,34 +15,4 @@ gulp.task('test', function(cb) {
         });
 });
 
-gulp.task('scripts-bundle', function (cb) {
-    var Duo = require('duo');
-    var fs = require('fs');
-    var typescript = require('./');
-    
-    var duo = new Duo(process.cwd())
-    duo
-      .entry('example/index.ts')
-      .development(true)
-      .use(typescript({ sourceMap: true, target: 'es6' }))
-      .run(function(err, src) {
-            if (err) {
-                gutil.log(err);
-                cb();
-            }
-            else {
-                var filename = 'build/build.js';
-                fs.writeFile(filename, src, function(err) {
-                    if (err) throw err;
-                    gutil.log('Generated ' + filename);
-                    cb();
-                });
-            }
-        });
-
-});
-
-
-gulp.task('scripts', ['scripts-bundle']);
-gulp.task('watch', ['watch-scripts']);
-gulp.task('default', ['scripts']);
+hub('./example/gulpfile.js');
